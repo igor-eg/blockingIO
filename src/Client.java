139366578de3f0c180a290b0fd5576fd0299217a
Igor Egorov <igor73@list.ru>
2022-05-20 14:27:41 +0300

    first commit

---
+++ src/Client.java
@@ -0,0 +1,26 @@
+import java.io.*;
+import java.net.Socket;
+import java.util.Scanner;
+
+public class Client {
+    private static final String localhost = "127.0.0.1";
+
+    public static void main(String[] args) throws IOException {
+        try (Socket socket = new Socket("127.0.0.1", 23444)) {
+            // Получаем входящий и исходящий потоки информации
+            try (BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+                 PrintWriter out = new PrintWriter(new OutputStreamWriter(socket.getOutputStream()), true);
+                 Scanner scanner = new Scanner(System.in)) {
+                String msg;
+                while (true) {
+                    System.out.println("Введите число для расчета значения N-го члена ряда Фибоначчи");
+                    msg = scanner.nextLine();
+                    out.println(msg);
+                    System.out.println("SERVER: " + in.readLine());
+                    if ("end".equals(msg)) break;
+                }
+            }
+        }
+
+    }
+}
---
+++ src/Server.java
@@ -0,0 +1,42 @@
+import java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class Server {
+
+    public static void main(String[] args) throws IOException {
+        System.out.println("Server started");
+        //  Занимаем порт, определяя серверный сокет
+        ServerSocket servSocket = new ServerSocket(23444);
+        while (true) {
+            //  Ждем подключения клиента и получаем поток и для дальнейшей работы
+            try (Socket socket = servSocket.accept();
+                 PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+                 BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()))) {
+                String line;
+                while ((line = in.readLine()) != null) {
+
+                    Integer j = Integer.valueOf(line);
+                    int[] arr = new int[j];
+                    arr[0] = 0;
+                    arr[1] = 1;
+                    for (int i = 2; i < arr.length; ++i) {
+                        arr[i] = arr[i - 1] + arr[i - 2];
+                    }
+                    for (int i = 0; i < arr.length; ++i) {
+                        System.out.println(arr[i]);
+                    }
+                    out.println("Значение N-го члена ряда Фибоначчи: " + arr[arr.length - 1]);
+
+                    if (line.equals("end")) {
+                        break;
+                    }
+                }
+
+            } catch (IOException e) {
+                e.printStackTrace(System.out);
+            }
+
+        }
+    }
+}
